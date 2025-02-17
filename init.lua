9ee4daa6cd149f69476b55e3fbe8484f7fbccfa0
Trey Eustache <trteust@gmail.com>
2025-02-17 12:42:56 -0500

    Changes the column limit to 105

--- init.lua
+++ init.lua
@@ -69,7 +69,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
-vim.cmd 'match Error /\\%>80c/'
+vim.cmd 'match Error /\\%>105c/'
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
