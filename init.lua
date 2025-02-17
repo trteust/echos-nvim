1216ac3ad8c0a2ca5cbb40498ee955b1ec983fcc
Trey Eustache <trteust@gmail.com>
2025-02-17 11:50:07 -0500

    Adds Vimwiki plugin.

--- init.lua
+++ init.lua
@@ -908,6 +908,24 @@ require('lazy').setup({
     end,
   },
 
+  -- vimwiki
+  {
+    'vimwiki/vimwiki',
+    branch = 'dev',
+    init = function()
+      print 'Setting up vimwiki!'
+      vim.g.vimwiki_list = {
+        {
+          template_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/vimwiki/autoload/',
+          syntax = 'default',
+          ext = '.wiki',
+          path = '~/vimwiki',
+        },
+      }
+      vim.g.vimwiki_global_ext = 0
+    end,
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
